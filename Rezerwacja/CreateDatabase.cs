63417f9d6cec138648188de52d0995d5f5bbaa58
Stolarz <tomek.solik@gmail.com>
2015-06-15 14:43:58 +0200

    First Commit

---
+++ Rezerwacja/CreateDatabase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLitePCL;
+namespace Rezerwacja
+{
+    class CreateDatabase
+    {
+        public static void LoadDatabase(SQLiteConnection db)
+        {
+            string sql = @"CREATE TABLE IF NOT EXISTS
+                                Rezerwacja (Id      INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                                            Imie    VARCHAR( 140 ),
+                                            Nazwisko    VARCHAR( 140 ),
+                                            Ilosc INTEGER,
+                                            Obiekt (VARCHAR( 140 ),
+                                            Data (VARCHAR( 140 ) 
+                            );";
+            using (var statement = db.Prepare(sql))
+            {
+                statement.Step();
+            }
+
+            sql = @"CREATE TABLE IF NOT EXISTS
+                                Obiekty (Id          INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                                         Nazwa        VARCHAR( 140 ),
+                                         Ilosc INTEGER
+                            )";
+            using (var statement = db.Prepare(sql))
+            {
+                statement.Step();
+            }
+
+            // Turn on Foreign Key constraints
+            sql = @"PRAGMA foreign_keys = ON";
+            using (var statement = db.Prepare(sql))
+            {
+                statement.Step();
+            }
+        }
+
+    }
+}
---
+++ Rezerwacja/RezerwacjaClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rezerwacja
+{
+    public class RezerwacjaClass
+    {
+        private long id=-1;
+        public long Id
+        {
+            get { return id;}
+            set {SetProperty(ref id,value);}
+        }
+
+
+
+
+        internal Rezerwacja(long id, string imie, string nazwisko, int ilosc, string obiekt, string data){
+            this.id = id;
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.ilosc = ilosc;
+            this.obiekt = obiekt;
+            this.data = data;
+        }
+    }
+}
