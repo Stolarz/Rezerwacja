8f9ef9712217fb0e285e90878ca003b9489f7dbb
Marcin Tomalak <marcintomalak1@gmail.com>
2015-06-20 13:35:33 +0200

    Wczytywanie rezerwacji do listy. Dodano możliwość tworzenia obiektów oraz wrzucania ich do bazy danych

--- Rezerwacja/DodajObiekt.xaml.cs
+++ Rezerwacja/DodajObiekt.xaml.cs
@@ -7,7 +7,8 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
-
+using SQLitePCL;
+using System.Diagnostics;
 namespace Rezerwacja
 {
     public partial class DodajObiekt : PhoneApplicationPage
@@ -15,6 +16,31 @@ namespace Rezerwacja
         public DodajObiekt()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection("database.db"))
+                {
+                    using (var statment = connection.Prepare(@"INSERT INTO Obiekty(ID,Nazwa,Ilosc) VALUES (?,?,?);"))
+                    {
+                        statment.Bind(1, 1);
+                        statment.Bind(2, ObiektNazwaTextbox.Text);
+                        statment.Bind(3, ObiektIloscTextbox.Text);
+                       
+                        statment.Step();
+                        statment.Reset();
+                        statment.ClearBindings();
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception\n" + ex.ToString());
+            }
         }
     }
 }
--- Rezerwacja/Rezerwacje.xaml.cs
+++ Rezerwacja/Rezerwacje.xaml.cs
@@ -7,7 +7,9 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
-
+using System.Collections.ObjectModel;
+using SQLitePCL;
+using Rezerwacja;
 namespace Rezerwacja
 {
     public partial class Rezerwacje : PhoneApplicationPage
@@ -15,6 +17,36 @@ namespace Rezerwacja
         public Rezerwacje()
         {
             InitializeComponent();
+        }
+
+
+        public class Rezerwacje
+        {
+            public string Imie { get; set; }
+            public string Nazwisko { get; set; }
+            public string Obiekt { get; set; }
+        }
+
+        public static ObservableCollection<Rezerwacje> getValues()
+        {
+            ObservableCollection<Rezerwacje> list = new ObservableCollection<Rezerwacje>();
+
+            using (var connection  = new SQLiteConnection("database.db"))
+            {
+                using(var statment = connection.Prepare(@"Select * FROM Rezerwacje;"))
+                {
+                    while(statment.Step() == SQLiteResult.ROW)
+                    {
+                        list.Add(new Rezerwacje()
+                        {
+                            Imie = (string)statment[1],
+                            Nazwisko = (string)statment[2],
+                            Obiekt = (string)statment[4]
+                        });
+                    }
+                }
+            }
+            return list;
         }
     }
 }
